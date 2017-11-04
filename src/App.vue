afa0e1f3b51c86d3f313de360859813e8828f6b7
randolphchin <randychin@163.com>
2017-11-05 00:14:45 +0800

    show navigator content by router

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,44 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/sellers">商家</router-link>
+      </div>
     </div>
-    <div class="content">
-      I am content
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import header from './components/header/header.vue'
-export default {
-  components: {
-    'v-header': header
+  import header from './components/header/header.vue'
+
+  export default {
+    components: {
+      'v-header': header
+    }
   }
-}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   #app
     .tab
-      display:flex
-      width : 100%
+      display: flex
+      width: 100%
       height: 40px
-      line-height :40px
+      line-height: 40px
       .tab-item
-        flex :1
+        flex: 1
         text-align: center
-
+        &  > a
+            display: block
+            font-size : 14px
+            color : rgb(77,85,93)
+            &.active
+              color: rgb(240,20,20)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import goods from './components/goods/goods.vue'
+import ratings from './components/ratings/ratings.vue'
+import sellers from './components/sellers/sellers.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', redirect: '/goods'},
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/sellers', component: sellers}
+]
+const router = new VueRouter({
+  linkActiveClass: 'active', /* router官方文档中的 router 构造配置中参数，效果为 '点击后高亮' */
+  routes // （缩写）相当于 routes: routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: {App},
+  router
 })
+
+/* router.go('/goods') 设置默认路由，这里使用 go 则浏览器会不断的刷新页面 */
