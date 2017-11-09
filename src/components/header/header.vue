04e5bb907eb3d483b23b3813217a51d9304a779f
randolphchin <randychin@163.com>
2017-11-10 00:30:07 +0800

    finish star.vue

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,15 +17,28 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div class="support-content" v-if="seller.supports">
+      <div class="support-content" v-if="seller.supports" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +48,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data () {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true
       }
     },
     created () {
@@ -47,8 +70,10 @@
   @import "../../common/stylus/mixin"
 
   .header
+    position:relative
     color: #fff
     background: rgba(7, 17 ,27, 0.5)
+    overflow: hidden
     .content-wrapper
       position:relative
       padding: 24px 12px 18px 24px
@@ -132,7 +157,7 @@
       .bulletin-title
         display: inline-block
         vertical-align: top
-        margin-top: 7px
+        margin-top: 8px
         width: 22px
         height: 12px
         bg-image('bulletin')
@@ -147,4 +172,40 @@
         font-size: 10px
         right: 12px
         top: 8px
+    .background
+      position:absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position: fixed
+      top: 0
+      left: 0
+      z-index: 100
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7 , 17 , 27 , 0.8)
+      .detail-wrapper
+        min-height: 100%
+        width: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+
+      .detail-close
+         position: relative
+         width: 32px
+         height: 32px
+         margin: -32px auto 0 auto
+         clear: both
+         font-size: 32px
 </style>
